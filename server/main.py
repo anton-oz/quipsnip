511d94968016bcdb8b856baee2bb6322f6b6413c
anton <antonosland789@gmail.com>
2024-09-15 10:51:00 -0500

    changed logo, started create demo page, simplified fastAPI run script

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,3 +8,8 @@ x = "Alone again, naturally."
 @app.get("/")
 async def root():
     return {"message": x}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", 
+                reload=True,
+                port=3001)
